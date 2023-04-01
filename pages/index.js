2c3931fbb92071a35f61e9fb637c112acf491c88
Abu Hanif <hanifcse@gmail.com>
2023-04-01 23:04:47 +0600

    Test puerpose

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
 import Footer from "@/components/Footer";
 
 import Head from "next/head";
-import { Inter } from "next/font/google";
-
-import AnimatedText from "react-animated-text-content";
-import Service from "@/components/service";
+import { Inter } from "next/font/google"
+import Service from "@/components/service/service";
+import Questions from "@/components/questions/Questions";
+import TakeService from "@/components/TakeService";
+import ContactForm from "@/components/ContactForm";
+import Navbar from "@/components/navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const mystyle = {
-    margin: "100px",
-    textAlign: "center",
-    fontSize: "100px",
-  };
+ 
   return (
     <>
       <Head>
@@ -24,49 +22,12 @@ export default function Home() {
       </Head>
       <Navbar></Navbar>
       <Service></Service>
-      {/* for  adding animated text used to react-animated-text package */}
-      <AnimatedText
-        type="bounce"
-        animation={{
-          x: "200px",
-          y: "-20px",
-          scale: 1.1,
-          ease: "ease-in-out",
-        }}
-        animationType="float"
-        interval={0.06}
-        duration={0.8}
-        tag="p"
-        className="animated-paragraph"
-        includeWhiteSpaces
-        threshold={0.1}
-        rootMargin="20%"
-        style={mystyle}
-      >
-        Let's F**k the Next
-      </AnimatedText>
-      ;
-      <AnimatedText
-        type="bounce"
-        animation={{
-          x: "200px",
-          y: "-20px",
-          scale: 1.1,
-          ease: "ease-in-out",
-        }}
-        animationType="wave"
-        interval={0.08}
-        duration={1}
-        tag="p"
-        className="animated-paragraph"
-        includeWhiteSpaces
-        threshold={0.1}
-        rootMargin="20%"
-        style={mystyle}
-      >
-        Build The HealthHub
-      </AnimatedText>
-      ;
+      <Questions></Questions>
+      <TakeService></TakeService>
+      <ContactForm></ContactForm>
+      <Footer></Footer>
+     
+    
     </>
   );
 }
