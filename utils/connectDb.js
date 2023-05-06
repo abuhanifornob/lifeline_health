9db691a1f2b8b20d857c9492aeb92de57846180e
Rakibul Hasan <rakibulhasan0641@gmail.com>
2023-05-06 13:59:05 +0600

    Install Axios , merge with SUbrota

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -1,21 +1,5 @@
 const mongoose = require("mongoose");
 
-<<<<<<< HEAD
-async function connectDb() {
-  try {
-    const MONGO_URI =
-      "mongodb+srv://lifeline:oagbheCCYxtBGuEs@cluster0.wfqwiph.mongodb.net/Lifeline?retryWrites=true&w=majority";
-    console.log("Db Connection Error", MONGO_URI);
-    // const blogsCollection = client.db('lifeline').collection('blogs');
-    await mongoose.connect(MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
-    console.log("connection succcess");
-  } catch (error) {
-    console.log(error);
-  }
-=======
 async function connectDb(){
     try {
         const MONGO_URI="mongodb+srv://lifeline:oagbheCCYxtBGuEs@cluster0.wfqwiph.mongodb.net/Lifeline?retryWrites=true&w=majority"
@@ -28,6 +12,5 @@ async function connectDb(){
     } catch (error) {
         console.log(error)
     }
->>>>>>> ff8693a17f8bb31a9b5eb063751d6a1527973ed7
 }
 module.exports = connectDb;
