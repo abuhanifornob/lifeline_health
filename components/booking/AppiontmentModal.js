7722e0dd583e53a4e0566e67f26ffeee21fcaac5
Rakibul Hasan <rakibulhasan0641@gmail.com>
2023-05-08 21:27:23 +0600

    Bokking modal open

--- components/booking/AppiontmentModal.js
+++ components/booking/AppiontmentModal.js
@@ -35,71 +35,72 @@ const AppointmentModal = () => {
                         <p className='text-center font-bold text-primary'>Available Appointments on {format(selectedDate, 'PP')}</p>
                     </div>
                 </div>
+                <div className="bg-gray-300 p-4">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="mb-4">
+                            <label
+                                className="block text-gray-700 font-bold mb-2"
+                                htmlFor="name"
+                            >
+                                Name
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="name"
+                                type="text"
+                                placeholder="John Doe"
+                                {...register('name', { required: true })}
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label
+                                className="block text-gray-700 font-bold mb-2"
+                                htmlFor="service"
+                            >
+                                Service
+                            </label>
+                            <select
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="service"
+                                {...register('service', { required: true })}
+                            >
+                                <option>Select a service</option>
+                                <option>Service 1</option>
+                                <option>Service 2</option>
+                                <option>Service 3</option>
+                            </select>
+                        </div>
+                        <div className="mb-4">
+                            <label
+                                className="block text-gray-700 font-bold mb-2"
+                                htmlFor="time"
+                            >
+                                Time Slot
+                            </label>
+                            <select
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="time"
+                                {...register('time', { required: true })}
+                            >
+                                <option>Select a time slot</option>
+                                <option>9:00 AM - 10:00 AM</option>
+                                <option>10:00 AM - 11:00 AM</option>
+                                <option>11:00 AM - 12:00 PM</option>
+                                <option>1:00 PM - 2:00 PM</option>
+                                <option>2:00 PM - 3:00 PM</option>
+                                <option>3:00 PM - 4:00 PM</option>
+                            </select>
+                        </div>
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="submit"
+                        >
+                            Book Now
+                        </button>
+                    </form>
+                </div>
             </div>
-            <div className="bg-gray-300 p-4">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="mb-4">
-                        <label
-                            className="block text-gray-700 font-bold mb-2"
-                            htmlFor="name"
-                        >
-                            Name
-                        </label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="name"
-                            type="text"
-                            placeholder="John Doe"
-                            {...register('name', { required: true })}
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label
-                            className="block text-gray-700 font-bold mb-2"
-                            htmlFor="service"
-                        >
-                            Service
-                        </label>
-                        <select
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="service"
-                            {...register('service', { required: true })}
-                        >
-                            <option>Select a service</option>
-                            <option>Service 1</option>
-                            <option>Service 2</option>
-                            <option>Service 3</option>
-                        </select>
-                    </div>
-                    <div className="mb-4">
-                        <label
-                            className="block text-gray-700 font-bold mb-2"
-                            htmlFor="time"
-                        >
-                            Time Slot
-                        </label>
-                        <select
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="time"
-                            {...register('time', { required: true })}
-                        >
-                            <option>Select a time slot</option>
-                            <option>9:00 AM - 10:00 AM</option>
-                            <option>10:00 AM - 11:00 AM</option>
-                            <option>11:00 AM - 12:00 PM</option>
-                            <option>1:00 PM - 2:00 PM</option>
-                            <option>2:00 PM - 3:00 PM</option>
-                            <option>3:00 PM - 4:00 PM</option>
-                        </select>
-                    </div>
-                    <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="submit"
-                    >
-                        Book Now
-                    </button>
-                </form>
-            </div>
+
         </div>
     );
 };
