4a97ae6aec7dec7712296316ebdad991ebc5dd05
Rakibul Hasan <rakibulhasan0641@gmail.com>
2023-05-09 08:56:39 +0600

    Error does not solve

--- pages/service/doctor-consultant/[service].js
+++ pages/service/doctor-consultant/[service].js
@@ -1,78 +1,82 @@
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
-// import slider from '../../../public/service-img/slider.jpg'
 import Image from 'next/image';
 import 'react-day-picker/dist/style.css';
 import DoctorCard from '@/components/service/doctor-card';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+const ServiceDetails = ({ experts }) => {
+    const router = useRouter();
+    const [isLoading, setLoading] = useState(false);
+    const [expert, setExpert] = useState([])
+    const user = {};
+    console.log(expert);
+
+    const { service } = router.query;
+    console.log(service);
+
+    useEffect(() => {
+        
+        const expertData = experts.find((expt) => expt.doctors.doctor[0].slug === service);
+        setExpert(expertData)
+        console.log(expert);
+    }, [router])
 
 
-const ServiceDetails = ( ) => {
-    const router = useRouter()
-    const [data, setData] = useState([])
-    const [isLoading, setLoading] = useState(false)
-    const user = {}
-    // console.log(electedExpert);
 
+    // const info = {
+    //     image: "",
+    //     name: expert.name,
+    //     title: expert.title,
+    //     specialty: expert.speciality,
+    //     experience: expert.exp,
+    //     rating: expert.rating,
+    //     numberOfRatings: expert.num,
+    //     fees: expert.fees,
+    // };
 
-
-    const { service } = router.query
-  
-    // console.log(date);
-
-    // console.log(service);
-
-
-
-    const info = { image: 'image', name: 'name', title: 'title', specialty: 'speciality', experience: 'exp', rating: 'rating', numberOfRatings: 'num', fees: '200' }
-
-    if (isLoading) return <p>Loading...</p>
-    if (!data) return <p>No   data</p>
+    if (isLoading) return <p>Loading...</p>;
+    if (!expert) return <p>No data</p>;
     return (
         <div>
             <div className="relative">
-                {/* <Image
-                    className="object-cover w-full object-top h-56 sm:h-96"
-                    src={slider}
-                    alt="slider"
-                ></Image> */}
+
                 <div className="absolute inset-0 bg-gray-900 bg-opacity-50" />
             </div>
-            <h2 className='text-5xl text-center py-10 font-bold'>{data.service}</h2>
+            <h2 className="text-5xl text-center py-10 font-bold">{expert.name}</h2>
 
-            <div>
-                <DoctorCard info={info}></DoctorCard>
+            <div>{
+                expert.map((info,idx)=>{<DoctorCard key={idx} info={info}></DoctorCard>
+                })
+            }
+                
             </div>
-  
-            <div  >
-                <h2 className='text-center text-3xl font-semibold'>Book Appointment</h2>
-
-            </div>
-            
-
-
-
-
         </div>
     );
 };
 
-
-
 export default ServiceDetails;
 
+export async function getServerSideProps() {
+    try {
+        const response = await axios.get(
+            'https://lifeline-health-rakibul181.vercel.app/api/expert'
+        );
+        const experts = response.data;
+        console.log(experts);
 
-// export async function  getServerSideProps() {
-//     const res = await fetch(`https://lifeline-health-rakibul181.vercel.app/api/expert`);
-//     const data = await res.json();
-//     const allExpert =  data.doctors.doctor;
-//     console.log(data);
-//     console.log(allExpert)
-//     const selectedExpert = allExpert.filter((expt) => expt.slug === service);
-  
-//     return {
-//       props: {
-//         selectedExpert
-//       },
-//     };
-    
-//   }
+        return {
+            props: {
+                experts,
+            },
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            props: {
+                experts: [],
+            },
+        };
+    }
+}
