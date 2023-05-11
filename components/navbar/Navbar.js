1a177fc51251eadbaaa44b3697dd4bffdd5ef2c1
subrota <subrota45278@gmail.com>
2023-05-11 10:44:58 +0600

    add navbar code

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -2,21 +2,21 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "../../public/logo1.png";
 import styles from "../../styles/Navbar.module.css";
+import DarkModeToggle from "../DarkMode/DarkModeToggle";
 import { useContext } from "react";
 import { AuthContext } from "@/context/AuthProvider";
-import { useRouter } from "next/router";
-// import DarkModeToggle from "../DarkMode/DarkModeToggle";
+import { toast } from "react-hot-toast";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Navbar() {
-  const {user,logout}=useContext(AuthContext)
-  const { push } = useRouter();
-  const handleLogOut = () => {
-    logout()
-        .then(() => { })
-        .catch(error => console.error(error))
-        push('/login')
-}
-  console.log('poooot',user?.photoURL)
+  const { user , logoutUser} = useContext(AuthContext);
+  function handleLogOut() {
+    logoutUser()
+   .then(() =>{
+    toast.success("Your profile log out successfully !!");
+    <Navigate to=""></Navigate>
+   } )
+  }
   return (
     <>
       <div className="navbar shadow-md  sticky top-0 z-10 bg-base-100">
@@ -43,9 +43,6 @@ function Navbar() {
               <li><Link className="text-lg font-medium" href={'/contact'}>Contact</Link></li>
               <li><Link className="text-lg font-medium" href={'/about'}>About Us</Link></li>
               <li><Link className="text-lg font-medium" href={'/blog'}>Blog</Link></li>
-              <li><Link className="text-lg font-medium" href={'/calculator'}>Health Calculatr</Link></li>
-              <li><Link className="text-lg font-medium" href={'/healthplans'}>Health Plans</Link></li>
-              <li><Link className="text-lg font-medium" href={'/DoctorRegistrationForm'}>For Doctors</Link></li>
               <li tabIndex={0}>
                 <a className="text-lg font-medium">
                   Services
@@ -66,7 +63,7 @@ function Navbar() {
                   <Link className="text-lg font-medium" href={"/service/health-plane"}>
                     <li>Fitness Expert</li>
                   </Link>
-                  
+
                 </ul>
               </li>
             </ul>
@@ -82,11 +79,7 @@ function Navbar() {
             <li><Link className="text-lg font-medium" href={'/contact'}>Contact</Link></li>
             <li><Link className="text-lg font-medium" href={'/about'}>About Us</Link></li>
             <li><Link className="text-lg font-medium" href={'/blog'}>Blog</Link></li>
-            <li><Link className="text-lg font-medium" href={'/chatpage'}>Live Chat </Link></li>
-        
-            <li><Link className="text-lg font-medium" href={'/calculator'}>Health Calculator</Link></li>
-            <li><Link className="text-lg font-medium" href={'/healthplans'}>Health Plans</Link></li>
-            <li><Link className="text-lg font-medium" href={'/doctorRegistrationForm'}>For Doctors</Link></li>
+
             <li tabIndex={0}>
               <Link href={""} className="text-lg font-medium">
                 Services
@@ -107,118 +100,72 @@ function Navbar() {
                 <Link className="text-lg font-medium" href={"/service/health-plane"}>
                   <li>Fitness expert</li>
                 </Link>
-                {/* <Link className="text-lg font-medium" href={"/service/gym-fitness-expert"}>
-                  <li>Gym & Fitness Expert</li>
-                </Link>
-                <Link className="text-lg font-medium" href={"/service/yoga-trainer"}>
-                  <li>Yoga Trainer</li>
-                </Link>
-                <Link className="text-lg font-medium" href={"/service/dentist"}>
-                  <li>Dentist</li>
-                </Link>
-                <Link className="text-lg font-medium" href={"/service/mental-helth"}>
-                  <li>Mental Helth</li>
-                </Link> */}
               </ul>
             </li>
-            <li> <a href="https://life-line-health-conference.firebaseapp.com/" target="_blank" rel="noopener noreferrer" className="btn btn-primary text-white float-right">Live video call</a> </li>
-           
-            {/* <li tabIndex={5}> <DarkModeToggle></DarkModeToggle> </li> */}
+
+
+      {
+     user &&   <>
+           <li tabIndex={1}>
+              <Link href={""} className="text-lg font-medium">
+                Go live
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                </svg>
+              </Link>
+              <ul className="p-2 bg-[#254747] absolute z-10 text-white">
+                <li>  <a href="https://life-line-health.netlify.app/videoCall.html" target="_blank" rel="noopener noreferrer" className="font-bold text-white float-right">Live video call</a> </li>
+                <li><Link className="text-lg font-medium" href={'/chatpage'}>Live Chat </Link></li>
+
+              </ul>
+            </li>
+
+        </>
+      }
+
+
+            <li tabIndex={5}> <DarkModeToggle></DarkModeToggle> </li>
           </ul>
         </div>
 
-        {/* <div className="navbar-center hidden lg:flex">
-    <ul className={`menu menu-horizontal px-1 ${styles.customMenu}`}>
-            <li><Link href={'/'}>Item 1</Link></li>
-            <li><Link href={'/'}>Item 2</Link></li>
-            <li><Link href={'/blog'}>Blog</Link></li>
-      <li tabIndex={0}>
-        <a>
-          Parent
-          <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-        </a>
-        <ul className="p-2">
-          <li><a>Submenu 1</a></li>
-          <li><a>Submenu 2</a></li>
-        </ul>
-      </li>
-    </ul>
-  </div> */}
+
 
         {/* navbar end   part  */}
         <div className="navbar-end gap-2">
-          {user?.photoUrl&&<>
+          {user && <>
             <div className="form-control">
-            <h2 className="text-[#254747] font-medium">{user?.displayName}</h2>
-          </div>
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full border-2 border-blue-400">
-                <img src={user?.photoURL} />
-              </div>
-            </label>
-            <ul tabIndex={0} className={`mt-3 p-2 shadow menu menu-compact dropdown-content bg-[#254747] text-white rounded-box w-52 ${styles.customMenu}`}>
-              <li>
-                <Link href={`/profile/${user.uid}`} className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </Link>
-              </li>
-              <li><a>My Appoinment</a></li>
-              <li><a>Settings</a></li>
-              {user?.uid&&<li onClick={handleLogOut}><a>Logout</a></li>}
-              {/* <li><a>Login</a></li> */}
-              
-            </ul>
-          </div>
-            </>}
+            </div>
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full border-2 border-blue-400">
+                  <img src={user?.photoUrl ? user?.photoUrl : "https://i.ibb.co/2Z3HNtT/usg.jpg"} width={100} height={100} alt="dfdfd" />
+                </div>
+              </label>
+
+              <ul tabIndex={0} className={`mt-3 p-2 shadow menu menu-compact dropdown-content bg-[#254747] text-white rounded-box w-52 ${styles.customMenu}`}>
+                <li><a>{user?.displayName ? user?.displayName : "username"}</a></li>
+                <li><a>My Appoinment</a></li>
+                <li><a>Settings</a></li>
+                <li onClick={() => handleLogOut()}><a>Logout</a></li>
+              </ul>
+            </div>
+          </>}
           {
-            user==="" && <>
-                     <ul tabIndex={0} className={`menu menu-horizontal flex gap-6 px-1 ${styles.customMenu}`}>
-              
-              <li> <Link href={"login"} className="text-lg font-medium">Login</Link></li> <li> <Link href={"login"} className="text-lg font-medium">Register</Link></li></ul>
+            !user && <>
+              <ul tabIndex={0} className={`menu menu-horizontal flex gap-6 px-1 ${styles.customMenu}`}>
+
+                <li> <Link href={"login"} className="text-lg font-medium">Login</Link></li> <li> <Link href={"login"} className="text-lg font-medium">Register</Link></li></ul>
 
             </>
           }
-           
         </div>
       </div>
-
-      {/* <div className=" max-w-screen-xl mx-auto ">
-        <div className="grid grid-cols-1 md:grid-cols-2 content-center min-h-screen">
-          <div className=" self-center">
-
-            <AnimatedText
-              type="words" // animate words or chars
-              animation={{
-                x: '200px',
-                y: '-20px',
-                scale: 1.1,
-                ease: 'ease-in-out',
-              }}
-              animationType="lights"
-              interval={0.06}
-              duration={0.8}
-              tag="p"
-              className={`animated-paragraph text-center lg:text-start text-5xl font-bold flex text-blue-500 mb-4`}
-              includeWhiteSpaces
-              threshold={0.1}
-              rootMargin="20%"
-            >
-              Lifeline Health
-            </AnimatedText>
-
-            <h1 className="text-3xl font-bold   "> The Complete Health Solution.We provide <span className="text-blue-500">any</span>  kind of <span className="text-blue-500">Health</span> solution </h1> <br />
-            <Link href={'/'} className="btn btn-primary ">Get Started</Link>
-
-
-
-          </div>
-          <div className="place-self-center">
-            <Image src={bannewrlogo} alt="health logo" width={1000} className={styles.animate}></Image>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 }
