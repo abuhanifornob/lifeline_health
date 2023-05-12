fd533a24353ee5b87bb411b31fb732f645a59402
subrota <subrota45278@gmail.com>
2023-05-12 16:16:46 +0600

    add updated navbar

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -77,13 +77,9 @@ function Navbar() {
         <div className="navbar-center hidden lg:flex">
           <ul className={`menu menu-horizontal flex gap-3 px-1 ${styles.customMenu}`}>
             <li><Link className="text-lg  " href={'/'}>Home</Link></li>
-            <li><Link className="text-lg  " href={'/contact'}>Contact</Link></li>
-            <li><Link className="text-lg  " href={'/about'}>About Us</Link></li>
-            <li><Link className="text-lg  " href={'/blog'}>Blog</Link></li>
-            <li><Link className="text-lg  " href={'/healthplans'}>Health Plan</Link></li>
-            <li><Link className="text-lg  " href={'/doctorRegistrationForm'}>For Doctor</Link></li>
+    
 
-            <li tabIndex={0}>
+            <li tabIndex={1}>
               <Link href={""} className="text-lg font-medium">
                 Services
                 <svg
@@ -109,7 +105,7 @@ function Navbar() {
 
             {
               user && <>
-                <li tabIndex={1}>
+                <li tabIndex={2}>
                   <Link href={""} className="text-lg font-medium">
                     Go live
                     <svg
@@ -132,10 +128,32 @@ function Navbar() {
                     }
                   </ul>
                 </li>
+                <li tabIndex={3}>
+              <Link href={""} className="text-lg font-medium">
+                Show more 
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                </svg>
+              </Link>
+              <ul className="p-2 bg-[#254747] absolute z-10 text-white">
+                <li><Link className="text-lg  " href={'/contact'}>Contact</Link></li>
+                <li><Link className="text-lg  " href={'/about'}>About Us</Link></li>
+                <li><Link className="text-lg  " href={'/blog'}>Blog</Link></li>
+                <li><Link className="text-lg  " href={'/healthplans'}>Health Plan</Link></li>
+                <li><Link className="text-lg  " href={'/doctorRegistrationForm'}>For Doctor</Link></li>
+
+              </ul>
+            </li>
 
               </>
             }
-            <li tabIndex={5}> <DarkModeToggle></DarkModeToggle> </li>
+            <li tabIndex={4}> <DarkModeToggle></DarkModeToggle> </li>
           </ul>
         </div>
 
@@ -147,13 +165,13 @@ function Navbar() {
             <div className="form-control">
             </div>
             <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <label tabIndex={5} className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full border-2 border-blue-400">
-                  <img src={user?.photoUrl ? user?.photoURL : "https://i.ibb.co/2Z3HNtT/usg.jpg"} width={100} height={100} alt="dfdfd" />
+                  <img src={user?.photoURL ? user?.photoURL : "https://i.ibb.co/2Z3HNtT/usg.jpg"} width={100} height={100} alt="dfdfd" />
                 </div>
               </label>
 
-              <ul tabIndex={0} className={`mt-3 p-2 shadow menu menu-compact dropdown-content bg-[#254747] text-white rounded-box w-52 ${styles.customMenu}`}>
+              <ul tabIndex={6} className={`mt-3 p-2 shadow menu menu-compact dropdown-content bg-[#254747] text-white rounded-box w-52 ${styles.customMenu}`}>
                 <li><a>{user?.displayName ? user?.displayName : "username"}</a></li>
                 <li><a>My Appoinment</a></li>
                 <li><a>Settings</a></li>
@@ -164,7 +182,7 @@ function Navbar() {
           </>}
           {
             !user && <>
-              <ul tabIndex={0} className={`menu menu-horizontal flex gap-6 px-1 ${styles.customMenu}`}>
+              <ul tabIndex={7} className={`menu menu-horizontal flex gap-6 px-1 ${styles.customMenu}`}>
 
                 <li> <Link href={"login"} className="text-lg font-medium">Login</Link></li> <li> <Link href={"login"} className="text-lg font-medium">Register</Link></li></ul>
 
